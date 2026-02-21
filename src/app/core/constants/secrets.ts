f2c133cb8cf25785a87c5cd9d10f4ad643f92e1d
Merdi-dev12 <merdipumba@gmail.com>
2026-02-21 03:50:20 -0800

    Delete src/app/core/constants/secrets.ts

--- src/app/core/constants/secrets.ts
+++
@@ -1,8 +0,0 @@
-export const secrets = {
-  supabaseUrl: 'https://eutsgxelosuuyckdfpae.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1dHNneGVsb3N1dXlja2RmcGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1OTI4NDQsImV4cCI6MjA4NzE2ODg0NH0.H-rjDh2o1DR32m4rRxi_cP0KkKEgL48tJ9XuUW0ZqTg',
-  production: false,
-  emailjsServiceId: 'service_mkpgxp4',
-  emailjsTemplateId: 'template_fwpw10e',
-  emailjsPublicKey: '4rnEU40JHHio313xT'
-};
